8d3d50abd8cbf59142fec74f9c6407fa919cdc1e
amf9t2 <amf9t2@mst.edu>
2014-12-07 21:05:56 -0600

    Reverted databse - SQLite - altamash

--- guestbookapp/models.py
+++ guestbookapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.forms import ModelForm
 
 # Create your models here.
+class guests(models.Model):
+    GuestID = models.BigIntegerField(primary_key=True, editable=False, serialize=True)
+    LastName = models.CharField(max_length=50, editable=True, serialize=True)
+    FirstName = models.CharField(max_length=50, editable=True, serialize=True)
+    IPAddress = models.IPAddressField(max_length=50, editable=False, serialize=True)
+    EntryDate = models.TimeField(auto_now=True, serialize=True)
+    OS = models.CharField(max_length=50, editable=False, serialize=True)
+    Device = models.CharField(max_length=50, editable=False, serialize=True)
+
+class GuestForm(ModelForm):
+    class Meta:
+        model = guests
+        fields = ['ID', 'Last Name', 'First Name', 'IPAddress', 'EntryDate', 'OS', 'Device']
--- guestbookapp/views.py
+++ guestbookapp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+
+from django.shortcuts import render_to_response
+from models import guest
 from django.http import  HttpResponse
+
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World!")
+    return render_to_response("guestbookapp/home.html", {'guest' : guest.objects.all()})
