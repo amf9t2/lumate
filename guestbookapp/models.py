1e6b3e3de41e8d8cb985437d6e4aacdab3fefae7
amf9t2 <amf9t2@mst.edu>
2014-12-07 23:32:28 -0600

    updated model- SQLite - Altamash

--- guestbookapp/models.py
+++ guestbookapp/models.py
@@ -3,11 +3,22 @@ from django.forms import ModelForm
 
 # Create your models here.
 class guests(models.Model):
-    GuestID = models.AutoField(primary_key=True)
-    LastName = models.CharField(max_length=50, editable=True, serialize=True)
-    FirstName = models.CharField(max_length=50, editable=True, serialize=True)
-    IPAddress = models.IPAddressField(max_length=50, editable=False, serialize=True)
-    EntryDate = models.TimeField(auto_now=True, serialize=True)
-    OS = models.CharField(max_length=50, editable=False, serialize=True)
-    Device = models.CharField(max_length=50, editable=False, serialize=True)
+    guest_id = models.AutoField(primary_key=True)
+    last_name = models.CharField(max_length=50, editable=True, serialize=True)
+    first_name = models.CharField(max_length=50, editable=True, serialize=True, null=True)
+    ip_address = models.IPAddressField(max_length=50, editable=False, serialize=True, null=True)
+    entry_date = models.TimeField(auto_now=True, serialize=True, null=True)
+    o_s = models.CharField(max_length=50, editable=False, serialize=True, null=True)
+    device = models.CharField(max_length=50, editable=False, serialize=True, null=True)
 
+def __str__(self):
+
+        return ' '.join([
+            self.guest_id,
+            self.last_name,
+            self.first_name,
+            self.ip_address,
+            self.entry_date,
+            self.o_s,
+            self.device
+        ])
